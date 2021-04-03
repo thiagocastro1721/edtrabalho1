2713c4c5a925765dd9ae1a8dd7216493a9534c04
thiagocastro1721 <thiagocastro1721@gmail.com>
2021-04-03 17:42:46 -0300

    sabado animado

--- edtrabalho1.py
+++ edtrabalho1.py
@@ -26,8 +26,14 @@ class Deque:
             sdeque += i
         return sdeque
     
+    def indice(self, indice):
+        return self.__items[indice]
+    
 deque = Deque()
-alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ "
+alfabeto = ""
+texto_cifrado = ""
+texto_decifrado = ""
+lista_decifrada = []
 
 '''
 def adicionar_alfabeto(deque, alfabeto):
@@ -35,16 +41,67 @@ Esta função adiciona ao objeto deque, do tipo Deque,
 cada caractere contido no parâmetro alfabeto (tipo string).
 '''
 def adicionar_alfabeto(deque, alfabeto):
-    for i in range(27):
+    #for para preencher a deque com a string alfabeto
+    for i in range(len(alfabeto)):
         deque.add_front(alfabeto[i])
-        
-    for i in range(3):
-        deque.remove_rear()
-        deque.add_front(alfabeto[i])
-        
-    print(alfabeto)
-    print(deque)
+    #depois que esta funcao eh executada a string alfabeto fica vazia
+    
+    #print(alfabeto)
+    #print(deque)
     #print(deque[0]) não deu certo
+
+def decifrar(deque, texto_cifrado, chave):
+    for i in range(len(texto_cifrado)):
+        for j in range(deque.size()):
+            if texto_cifrado[i] == deque.indice(j):
+                indice_novo = j - chave
+                #print("indice i = %d" % (i))
+                #print("letra do texto cifrado no incice i %d = %s" % (i, texto_cifrado[i]))
+                #print("indice j = %d" % (j))
+                #print("elemento da deque no indice %d = %s" % (j, deque.indice(j)))
+                #print("novo_indice = %d" % (indice_novo))
+                #print(lista_decifrada)
+                lista_decifrada.append(deque.indice(indice_novo))
+                #texto_decifrado.replace(texto_decifrado[], deque.indice(indice_novo))
+    texto_cifrado = ''.join(map(str, lista_decifrada))#unir elementos de uma lista e transforma-la em uma string
+    lista_decifrada.clear()#limpado a lista
+    return texto_cifrado
     
     
-adicionar_alfabeto(deque, alfabeto)
+    
+    #embaralhar de acordo com a chave
+    #for i in range(chave):#nao eh possivel fazer pois alfabeto esta vazio
+    #    deque.remove_rear()
+    #    deque.add_front(deque.indice(i))
+#alfabeto = deque
+#p#int(deque)
+def selecionar_subconjunto_missoes():#todo o resto da implimentação deve estar dentro desta funcao
+    lista_de_strings = []
+    horas_disponiveis = int(input())
+    apresentar_missoes_s = int(input())#1 para sim e 0 para não
+    indice_da_coluna = int(input())
+    alfabeto = input()
+    chave = int(input())
+    missoes_cifradas = int(input())
+    
+    adicionar_alfabeto(deque, alfabeto)
+    #Capturar N strings
+    for i in range(missoes_cifradas):
+        string_aux = ""
+        string_colchetes = "[]"
+        string_aux = input()
+            
+        #for para remover os colchetes na primeira e ultima posição
+        for i in range(0,len(string_colchetes)):
+            string_aux = string_aux.replace(string_colchetes[i],"")
+            #print("String_aux = %s" % (string_aux))
+        lista_de_strings.append(string_aux)
+    
+    #decifrar N strings
+    for i in range(missoes_cifradas - 1, -1, -1):
+        #print("Numero de for's = %d" % (i))
+        texto_cifrado = lista_de_strings[i]
+        #print("Texto cifrado = %s" % (texto_cifrado))
+        print(decifrar(deque, texto_cifrado, chave))
+    #print(lista_de_strings)
+         
