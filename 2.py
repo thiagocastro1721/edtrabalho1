6710e28645406da9989362c69236f3fa6501a9ed
thiagocastro1721 <thiagocastro1721@gmail.com>
2021-04-04 10:04:30 -0300

    mochila

---
+++ 2.py
@@ -0,0 +1,43 @@
+def mochila(n, v, w, W):
+    usa = 0
+    nao_usa = 0
+    n = n + 1
+    W = W + 1
+    # W capacidade da mochila
+    # j linhas
+    # x colunas
+    # n quantidade de itens
+    # w lista com os pesos dos itens
+    # v lista com os valores dos itens
+    # M matriz
+    
+    # Definiindo a Matriz com n linhas e W colunas
+    # Preenchendo com zeros
+    # Para acessar o elemento matriz[2][3] devemos subtrair 1
+    # da linha e da coluna, Assim matriz[1][2].
+    # pois o indice comeca com zero
+    matriz = []
+    for i in range(n):
+        matriz.append([0]*W)
+    #print(matriz)
+   
+    # Sequencia de 2 for's para zerar a primeira linha
+    # e a primeira coluna
+    for x in range(W):
+        matriz[0][x] = 0
+    for j in range(n):
+        matriz[j][0] = 0
+
+    for j in range(1, n):
+        for x in range(W):
+            if w[j - 1] > x:
+                matriz[j][x] = matriz[j - 1][x]
+            else:
+                usa = v[j - 1] + matriz[j - 1][x - w[j - 1]]
+                nao_usa = matriz[j - 1][x]
+                if usa > nao_usa:
+                    matriz[j][x] = usa
+                else:
+                    matriz[j][x] = nao_usa
+    print(matriz)
+    return matriz[n -1][W - 1]
